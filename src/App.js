9a5750c6f7231044f4bb6fbef8b0dad51676acd8
Stahnev Yury <stahnev@stael.ru>
2022-11-07 13:04:14 +0300

    small edits after refactor + prettier formatted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Link, Redirect, Route, useParams, Switch} from 'react-router-dom';
+import { Link, Redirect, Route, useParams, Switch } from 'react-router-dom';
 
 const users = [0, 1, 2, 3, 4];
-const isUserExist = (userId) => users.some(uid => uid === Number(userId));
+const isUserExist = (userId) => users.some((uid) => uid === Number(userId));
 
 function MainPage() {
    return (
       <>
          <h2>Main Page</h2>
-
       </>
    );
 }
@@ -17,15 +16,21 @@ function UsersLayout() {
    return (
       <>
          <h2>Users Layout</h2>
-         <p><Link to='/'>Main Page</Link></p>
+         <p>
+            <Link to="/">Main Page</Link>
+         </p>
          <Switch>
-            <Route path='/users' exact component={UsersList} />
-            <Route path='/users/:userId/profile' exact component={UserPage} />
-            <Route path='/users/:userId/edit' exact component={UserEditPage} />
-            <Redirect to='/users/:userId/profile' />
+            <Route path="/users" exact component={UsersList} />
+            <Route
+               path="/users/:userId/profile"
+               exact
+               component={UserProfilePage}
+            />
+            <Route path="/users/:userId/edit" exact component={UserEditPage} />
+            <Redirect to="/users/:userId/profile" />
          </Switch>
       </>
-   )
+   );
 }
 
 function UsersList() {
@@ -33,24 +38,31 @@ function UsersList() {
       <>
          <h2>User List Page</h2>
          <ul>
-            {users.map(uid => <li key={uid}><Link to={`/users/${uid}`}>{`User ${uid}`}</Link></li>)}
+            {users.map((uid) => (
+               <li key={uid}>
+                  <Link to={`/users/${uid}`}>{`User ${uid}`}</Link>
+               </li>
+            ))}
          </ul>
       </>
    );
 }
 
+function UserProfilePage() {
+   const { userId } = useParams();
 
-function UserPage() {
-   const {userId} = useParams();
-
-   if (!isUserExist(userId)) return <Redirect to={'/users'}/>
+   if (!isUserExist(userId)) return <Redirect to={'/users'} />;
 
    return (
       <>
          <h2>User Page</h2>
          <ul>
-            <li><Link to='/users'>Users List page</Link></li>
-            <li><Link to={`/users/${userId}/edit`}>Edit this user</Link></li>
+            <li>
+               <Link to="/users">Users List page</Link>
+            </li>
+            <li>
+               <Link to={`/users/${userId}/edit`}>Edit this user</Link>
+            </li>
          </ul>
          <p>{`userId: ${userId}`}</p>
       </>
@@ -58,33 +70,45 @@ function UserPage() {
 }
 
 function UserEditPage() {
-   const {userId} = useParams();
+   const { userId } = useParams();
 
-   if (!isUserExist(userId)) return <Redirect to={'/users'}/>
+   if (!isUserExist(userId)) return <Redirect to={'/users'} />;
 
    return (
       <>
          <h2>Edit User Page</h2>
          <ul>
-            <li><Link to={`/users/${userId}`}>User profile Page</Link></li>
-            <li><Link to={isUserExist(Number(userId) + 1) ? `/users/${Number(userId) + 1}` : `/users/${users[0]}`}>Another User</Link>
+            <li>
+               <Link to={`/users/${userId}`}>User profile Page</Link>
             </li>
-            <li><Link to='/users'>Users List page</Link></li>
+            <li>
+               <Link
+                  to={
+                     isUserExist(Number(userId) + 1)
+                        ? `/users/${Number(userId) + 1}`
+                        : `/users/${users[0]}`
+                  }
+               >
+                  Another User
+               </Link>
+            </li>
+            <li>
+               <Link to="/users">Users List page</Link>
+            </li>
          </ul>
       </>
    );
 }
 
-
 function App() {
-
-   return (<>
+   return (
+      <>
          <h1>App Layout v5</h1>
-         <Link to='/users'>Users list Page</Link>
+         <Link to="/users">Users list Page</Link>
          <Switch>
-            <Route path="/" exact component={MainPage}/>
-            <Route path='/users/:userId?/:page?' component={UsersLayout}/>
-            <Redirect from='*' to='/' />
+            <Route path="/" exact component={MainPage} />
+            <Route path="/users/:userId?/:page?" component={UsersLayout} />
+            <Redirect from="*" to="/" />
          </Switch>
       </>
    );
